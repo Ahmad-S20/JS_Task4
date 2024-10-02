98c5ffb73f48182fe48b343a3de499cf888bd047
Ahmad-S20 <seifahmad6363@gmail.com>
2024-10-02 19:18:39 +0300

    JS_Task4

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+async function getPizza(){
+    const response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=pizza`);
+    const result = await response.json();
+    //console.log(result);
+
+    const data = result.recipes;
+    
+   const posts = data.map(function(e){
+
+    return`<div class="product" >
+        <h2>${e.title}</h2>
+        <img src="${e.image_url}" alt="${e.title}">
+        </div>`;
+}).join(``);
+    console.log(posts);
+    document.querySelector(".products .row").innerHTML = posts;
+}
+
+getPizza();
